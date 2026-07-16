85a656551606959df52da45cfc74a97bde432717
lana <marilan171191@gmail.com>
2026-07-16 23:47:39 +0400

    Додаткові зміни

--- lesson_12/test_homeworks12.py
+++ lesson_12/test_homeworks12.py
@@ -6,13 +6,16 @@ from homeworks import check_unique_symbols
 from homeworks import check_word
 from homeworks import reverse_text
 
-# 1. tree_quantity
+class TestHomeworks(unittest.TestCase):
+
+    # 1. tree_quantity
     def test_tree_quantity_positive(self):
         self.assertEqual(tree_quantity(4), 15)
 
     def test_tree_quantity_negative_type(self):
         with self.assertRaises(TypeError):
             tree_quantity("String")
+
 
     # 2. count_apostrophes
     def test_count_apostrophes_positive(self):
@@ -21,6 +24,7 @@ from homeworks import reverse_text
     def test_count_apostrophes_without_apostrophe(self):
         self.assertEqual(count_apostrophes("Alice book"), 0)
 
+
     # 3. check_unique_symbols
     def test_check_unique_symbols_positive(self):
         self.assertTrue(check_unique_symbols("abcdefghijkl"))
@@ -28,12 +32,14 @@ from homeworks import reverse_text
     def test_check_unique_symbols_negative(self):
         self.assertFalse(check_unique_symbols("aaaaaa"))
 
+
     # 4. check_word
     def test_check_word_positive(self):
         self.assertTrue(check_word("horizon"))
 
     def test_check_word_negative(self):
         self.assertFalse(check_word("apple"))
+
 
     # 5. reverse_text
     def test_reverse_text_positive(self):
@@ -44,5 +50,5 @@ from homeworks import reverse_text
             reverse_text(13)
 
 
-    if __name__ == "__main__":
-            unittest.main()
+if __name__ == "__main__":
+    unittest.main()
